23feb2d6cddf96570068e432b9cba03adf7d44de
OBRA MIL S.A <alfonso.marrero98@gmail.com>
2024-01-23 18:58:27 -0300

    seals model

---
+++ models/Seals.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const SealSchema = new mongoose.Schema({
+  size: {
+    type: String,
+    maxlength: 80,
+    required: [true, "Ingrese Medida"],
+  },
+  code: {
+    type: Number,
+    required: [true, "Ingrese Codigo"],
+    unique: [true, "Código Existente"],
+  },
+  type: { type: String, maxlength: 20 },
+  price: { type: Number, required: [true, "Ingrese Precio"] },
+});
+
+module.exports = mongoose.model("Seal", SealSchema);
